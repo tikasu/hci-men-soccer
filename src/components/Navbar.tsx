b4f03e0243fd960cc6ce9f24fae38f722d580a91
tikasu <thinh_t_nguyen@yahoo.com>
2025-03-04 23:44:19 -0600

    Fix navbar: keep HCI Soccer text and add Rules link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useAuth } from '@/lib/hooks/useAuth';
 
